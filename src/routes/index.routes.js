3c58acaf07146d6534df068032fc57fca0faa899
RicardoSilvaF <zanphs@gmail.com>
2023-04-25 12:50:20 -0300

    Criacao das routes

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { register, login } from "../controllers/controller";
 
 const router = Router();
 
-
+router.post("/cadastro", register)
+router.post("/", login)
 
 export default router;
